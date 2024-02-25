000b2a344f9a0353fa045a0dd7da6712877ca7b1
Gabriel Massera <gmaszera@hotmail.com>
2024-02-25 20:51:07 -0300

    Tabuleiro UI e lógica

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -2,9 +2,13 @@ package org.example;
 
 
 import org.example.boardgame.Board;
+import org.example.chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
+        Board board = new Board(8, 8);
 
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
